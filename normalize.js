1b27dba3f42292b6f7505000297dfad740a4b791
Justin Covell <jujugoboom@gmail.com>
2020-10-18 22:09:27 -0700

    Change to use ReplayGain 2.0

--- normalize.js
+++ normalize.js
@@ -1,3 +1,8 @@
+
+/*
+    Attempt at realtime normalization using ReplayGain 2.0 and calculating LUFS based on algorithm found at https://github.com/SebastianZimmer/LoudEv
+*/
+
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var videoElements = document.getElementsByTagName("video");
 if (videoElements.length < 0) {
@@ -5,10 +10,94 @@ if (videoElements.length < 0) {
     console.log("video not loaded");
 } else {
     var audioSrc = audioCtx.createMediaElementSource(videoElements[0]);
-    var url = chrome.runtime.getURL('replay-gain-processor.js');
-    audioCtx.audioWorklet.addModule(url).then(() => {
+    var ebu_splitter = audioCtx.createChannelSplitter(2);
+
+    //first stage shelving filter
+    var highshelf_filter_L = audioCtx.createBiquadFilter();
+    highshelf_filter_L.type = "highshelf";
+    highshelf_filter_L.Q.value = 1;
+    highshelf_filter_L.frequency.value = 1500;
+    highshelf_filter_L.gain.value = 4;
+
+    var highshelf_filter_R = audioCtx.createBiquadFilter();
+    highshelf_filter_R.type = "highshelf";
+    highshelf_filter_R.Q.value = 1;
+    highshelf_filter_R.frequency.value = 1500;  //deduced with IIRFilter.getFrequencyResponse
+    highshelf_filter_R.gain.value = 4;
+
+    // second stage highpass filter
+    var highpass_filter_L = audioCtx.createBiquadFilter();
+    highpass_filter_L.frequency.value = 76;
+    highpass_filter_L.Q.value = 1;
+    highpass_filter_L.type = "highpass";
+
+    var highpass_filter_R = audioCtx.createBiquadFilter();
+    highpass_filter_R.frequency.value = 76;
+    highpass_filter_R.Q.value = 1;
+    highpass_filter_R.type = "highpass";
+
+    //SQUARING EVERY CHANNEL
+    var ebu_square_gain_L = audioCtx.createGain();
+    ebu_square_gain_L.gain.value = 0;
+
+    var ebu_square_gain_R = audioCtx.createGain();
+    ebu_square_gain_R.gain.value = 0;
+
+    var ebu_convolver_L = audioCtx.createConvolver();
+    ebu_convolver_L.normalize = false;
+    var ebu_convolver_R = audioCtx.createConvolver();
+    ebu_convolver_R.normalize = false;
+
+    var ebu_mean_gain_L = audioCtx.createGain();
+    ebu_mean_gain_L.gain.value = 1/(audioCtx.sampleRate * 3);
+    var ebu_mean_gain_R = audioCtx.createGain();
+    ebu_mean_gain_R.gain.value = 1/(audioCtx.sampleRate * 3);
+
+    var ebu_channel_summing_gain = audioCtx.createGain();
+    //CONNECTING EBU GRAPH
+    ebu_splitter.connect(highshelf_filter_L, 0, 0);
+    ebu_splitter.connect(highshelf_filter_R, 1, 0);
+
+    highshelf_filter_L.connect(highpass_filter_L);
+    highshelf_filter_R.connect(highpass_filter_R);
+
+    highpass_filter_L.connect(ebu_square_gain_L);
+    highpass_filter_L.connect(ebu_square_gain_L.gain);
+
+    highpass_filter_R.connect(ebu_square_gain_R);
+    highpass_filter_R.connect(ebu_square_gain_R.gain);
+
+    ebu_square_gain_L.connect(ebu_convolver_L).connect(ebu_mean_gain_L);
+    ebu_square_gain_R.connect(ebu_convolver_R).connect(ebu_mean_gain_R);
+
+    //Sum the signal
+    ebu_mean_gain_L.connect(ebu_channel_summing_gain);
+    ebu_mean_gain_R.connect(ebu_channel_summing_gain);
+
+    fetch(chrome.runtime.getURL("3sec-1-mono_44100.wav"))
+    .then(r => r.arrayBuffer())
+    .then(b => audioCtx.decodeAudioData(b))
+    .then(audioBuffer => {
+        ebu_convolver_L.buffer = audioBuffer;
+        ebu_convolver_R.buffer = audioBuffer;
+    }).then(() => {
+        var url = chrome.runtime.getURL('replay-gain-processor.js');
+        audioCtx.audioWorklet.addModule(url).then(() => {
         const replayGainNode = new AudioWorkletNode(audioCtx, "replay-gain-processor");
-        audioSrc.connect(replayGainNode);
+        audioSrc.connect(ebu_splitter);
+        var audio_data_merger = audioCtx.createChannelMerger(2);
+        ebu_channel_summing_gain.connect(audio_data_merger, 0, 0);
+        audioSrc.connect(audio_data_merger, 0, 1);
+        audio_data_merger.connect(replayGainNode);
         replayGainNode.connect(audioCtx.destination);
     }).catch((e) => console.log(e));
+    });
+
+    audioSrc.mediaElement.onpause = () => {
+        audioCtx.suspend();
+    }
+
+    audioSrc.mediaElement.onplay = () => {
+        audioCtx.resume();
+    }
 }
--- replay-gain-processor.js
+++ replay-gain-processor.js
@@ -1,31 +1,5 @@
 /*
-    AudioWorklet modification of ReplayGain algorithm found here https://github.com/est31/js-audio-normalizer
-
-    More information can be found in the ReplayGain specification http://wiki.hydrogenaud.io/index.php?title=ReplayGain_specification
-
-    Permission is hereby granted, free of charge, to any
-    person obtaining a copy of this software and associated
-    documentation files (the "Software"), to deal in the
-    Software without restriction, including without
-    limitation the rights to use, copy, modify, merge,
-    publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software
-    is furnished to do so, subject to the following
-    conditions:
-
-    The above copyright notice and this permission notice
-    shall be included in all copies or substantial portions
-    of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-    PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-    SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-    IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-    DEALINGS IN THE SOFTWARE.
+    AudioWorklet modification of LiveEV algorithm found https://github.com/SebastianZimmer/LoudEv implementing an approximation of ReplayGain 2.0
 */
 
 class ReplayGainProcessor extends AudioWorkletProcessor {
@@ -46,37 +20,39 @@ class ReplayGainProcessor extends AudioWorkletProcessor {
     
         var s = 0.0;
         for (var i = 0; i < inputData.length; i++) {
-            s += inputData[i] ** 2;
+            s += -0.691 + (10 * Math.log10(inputData[i]));
         }
         const sumCallback = (acc, curr) => acc + curr;
         this.sumBuffer.push(s);
+        if (s / inputData.length > -14) {
+            var audioInput = inputBuffer[1];
+            for (var channel = 0; channel < outputBuffer.length; channel++) {
+                var outputData = outputBuffer[channel];
+                for (var i = 0; i < audioInput.length; i++) {
+                    outputData[i] = audioInput[i]
+                }
+            }
+            return true;
+        }
         var sum = this.sumBuffer.reduce(sumCallback);
-        this.peakLevelBuffer.push(Math.sqrt(sum / (this.sumBuffer.length * inputData.length)));
-        if(this.sumBuffer.length > 16) {
-            // Shift buffer if it gets over 50ms of samples
+        var avgLoudness = sum / (this.sumBuffer.length * inputData.length);
+        if(this.sumBuffer.length > 1000) {
+            // Moving window buffer
             this.sumBuffer.shift()
         }
-        var sortedPeakLevel = [...this.peakLevelBuffer].sort(function(a, b) { return a - b; });
-        // Want to be able to react to large changes quickly, but not sure how (e.g. beginning of https://www.youtube.com/watch?v=ucZl6vQ_8Uo)
-        // if (Math.abs(this.peakLevelBuffer[this.peakLevelBuffer.length - 1] - sortedPeakLevel[sortedPeakLevel.length - 1]) > this.peakLevelBuffer[this.peakLevelBuffer.length - 1]**2 && this.peakLevelBuffer.length > 600) {
-        //     console.log("Clearing peak buffer");
-        //     var last = this.peakLevelBuffer[this.peakLevelBuffer.length - 1];
-        //     this.peakLevelBuffer = [last];
-        //     sortedPeakLevel = [last];
-        // }
-        var a = sortedPeakLevel[Math.floor(sortedPeakLevel.length * 0.95)]
-        if(this.peakLevelBuffer.length > 640) {
-            this.peakLevelBuffer.shift()
+        var rg = -14 - avgLoudness;
+        var gain = 10 ** (rg/20);
+        if (rg === -Infinity) {
+            gain = 1;
         }
-        var gain = 1.0 / a;
         gain = Math.max(gain, 0.02);
-        gain = Math.min(gain, 100.0);
-        gain = gain / 10.0;
-        // console.log("A: ", a)
+        gain = Math.min(gain, 5);
+        var audioInput = inputBuffer[1];
+        // console.log("Loudness/gain", avgLoudness, gain)
         for (var channel = 0; channel < outputBuffer.length; channel++) {
             var outputData = outputBuffer[channel];
-            for (var i = 0; i < inputData.length; i++) {
-                outputData[i] = inputData[i] * gain;
+            for (var i = 0; i < audioInput.length; i++) {
+                outputData[i] = audioInput[i] * gain;
             }
         }
         return true;
